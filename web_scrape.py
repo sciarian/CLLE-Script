908041d370ff21795d96ff3321c141b944ce1e7d
Chen Lab <chenlab@user-476d75.user.msu.edu>
2018-05-31 14:17:41 -0400

    refactored code to a object oriented paradigm

--- web_scrape.py
+++ web_scrape.py
@@ -1,61 +1,56 @@
-###############################################
-#This program will run a script that will
+################################################
+#This program will run a script that will 
 #search for the age, gender, and year of the 
 #earliest mention of a cell line. 
 #
 #@Author: Anthony Sciarini
 #@Version: 5/30/2018
-###############################################
+################################################
 
-#IMPORTED LIBS
+###############
+#IMPORTED LIBS#
+###############
 import requests				#Used send HTTP request
 from bs4 import BeautifulSoup	        #Used to process HTML documents
 
-#########################
-#	GET HTML PAGE	#
-#########################
+##########################################################################################
+#This class contains all the functions needed to scrap cell line info rom html documents.
+##########################################################################################
+class Cell_line_scraper:
 
-#Requests a HTTP page
-req = requests.request('GET','https://web.expasy.org/cellosaurus/CVCL_1058')
+	#############
+	#Constructor# ~ This guy will take the http URL and convert it to HTML TODO
+	#############
+	def __init__(self, url):
 
-#Grabs the HTML page that came from the request
-page = BeautifulSoup(req.content, 'html.parser')
+		#Make HTTP request
+		req = requests.request('GET', url)
 
-#Prints HTML page
-#print page
+		#Grab the HTML doc that came with request
+		self.page = BeautifulSoup(req.content, 'html.parser')
 
-###TODO LIST###
-#-GRAB AGE
-#-GRAB GENDER
-#-GRAB RACE
-#-GRAB YEAR ***
-###############
+	#########				
+	#Funtion# ~ Look up the content stored at a table row
+	#########
+	def table_look_up(self, table_row):	
+			
+		#Loop through each row in the page's table
+		for row in self.page.find_all('tr'):
+			if str(row.th) != 'None':
+				if str(row.th.string) == table_row:
+					print row.th.string + ' : ' + row.td.contents[0].string	
 
-#########################
-#	SCRAPE DOC	#
-#########################
+##################################
+####################  RUN PROGRAM 	
+####################	  	    
+####################		   
+##################################
 
-#--------------------------------------------------------
-#Look up each attribute in the expasy table by table row
-#--------------------------------------------------------
+###Create Cell line scraper for document
+obj = Cell_line_scraper("https://web.expasy.org/cellosaurus/CVCL_1058")
 
-#Loop through each row on the expasy table
-for row in page.find_all('tr'):
-
-	#Makes sure the row is not blank
-	if str(row.th) != 'None':
-
-		#Print the cell line
-		if row.th.string == 'Cell line name':
-			print row.th.string + ' : ' + row.td.contents[0].string
-
-		#Print the Synonyms for he cell
-		if row.th.string == 'Synonyms':
-			print row.th.string + ' : ' + row.td.contents[0].string
-	
-		#Print the Sex of the cell
-		if row.th.string == 'Sex of cell':
-			print row.th.string + ' : ' + row.td.contents[0].string
-		
-		if row.th.string == 'Age at sampling':
-			print row.th.string + ' : ' + row.td.contents[0].string
+###Print cell info
+obj.table_look_up('Cell line name')
+obj.table_look_up('Synonyms')
+obj.table_look_up('Sex of cell')
+obj.table_look_up('Age at sampling')
